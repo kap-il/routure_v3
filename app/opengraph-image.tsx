636d3f1d381ef1105477caff50732ee1f88c8464
kap-il <kapilr3500@gmail.com>
2026-03-31 18:32:47 -0400

    QOL fix for share favicon

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -24,7 +24,7 @@ export default async function Image() {
           src="https://routuremag.vercel.app/images/routure-logo.png"
           alt=""
           width={400}
-          height={90}
+          height={121}
         />
       </div>
     ),
