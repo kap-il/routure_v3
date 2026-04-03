fc103235f5e92722f43cb4f23cfe7d5eb8dc3b29
kap-il <kapilr3500@gmail.com>
2026-04-02 21:41:11 -0400

    contact link

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -14,7 +14,7 @@ const footerLinks = {
   ],
   company: [
     { name: 'About', href: '/coming-soon' },
-    { name: 'Contact', href: '/coming-soon' },
+    { name: 'Contact', href: '/community' },
     { name: 'Careers', href: '/coming-soon' },
   ],
 };
